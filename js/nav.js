b40f976686e87a31a72326f9a402289b6a8b27de
Piotr Osoba <piotrosoba@outlook.com>
2019-07-02 14:42:59 +0200

    formated

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,20 @@
-const nav = $('.nav');
+const nav = $(".nav");
 $(window).resize(function() {
-    if ($(window).width() < 785) {
-        nav.hide()
-    } else {
-        nav.show()
-    }
-})
-
-$('.sm-nav').click(function() {
-    nav.slideDown();
-})
-
-$('.nav__sm-close').click(function() {
-    nav.slideUp();
-})
-
-$('.nav__link').click(function() {
+  if ($(window).width() < 785) {
     nav.hide();
-})
+  } else {
+    nav.show();
+  }
+});
+
+$(".sm-nav").click(function() {
+  nav.slideDown();
+});
+
+$(".nav__sm-close").click(function() {
+  nav.slideUp();
+});
+
+$(".nav__link").click(function() {
+  nav.hide();
+});
