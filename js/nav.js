fd817f1c33c432b0050cded3521d0bf3aae625d7
Piotr Osoba <piotrosoba@outlook.com>
2019-07-12 15:34:15 +0200

    added hamburger icon

--- js/nav.js
+++ js/nav.js
@@ -2,28 +2,27 @@
   const nav = document.querySelector('.nav')
   const media = window.matchMedia('(max-width: 785px)')
   const navLink = document.querySelectorAll('.nav__link')
+  const smNav = document.querySelector('.sm-nav')
+  const navSmClose = document.querySelector('.nav__sm-close')
 
   window.addEventListener('resize', function() {
     if (media.matches) {
-      nav.style.height = '0px'
-      nav.style.display = 'none'
+      nav.style.height = '200px'
+      nav.style.top = '-200px'
     } else {
       nav.style.height = '80px'
       nav.style.display = 'block'
+      nav.style.top = '0'
     }
   })
 
-  document.querySelector('.sm-nav').addEventListener('click', function() {
-    nav.style.height = '200px'
-    nav.style.display = 'block'
+  smNav.addEventListener('click', function() {
+    nav.style.top = '0'
   })
 
-  document
-    .querySelector('.nav__sm-close')
-    .addEventListener('click', function() {
-      nav.style.height = '0px'
-      nav.style.display = 'none'
-    })
+  navSmClose.addEventListener('click', function() {
+    nav.style.top = '-200px'
+  })
 
   navLink.forEach(function(link) {
     link.addEventListener('click', function(evt) {
@@ -66,7 +65,7 @@
   window.addEventListener('scroll', function() {
     let navHeight = 150
     if (media.matches) {
-      nav.hide()
+      nav.style.top = '-200px'
       navHeight = 100
     }
 
